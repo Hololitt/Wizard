7bf1d46e13deab4b0732ba9b28d8bb88dd7e8543
Nikon <109473540+Hololitt@users.noreply.github.com>
2026-02-02 20:44:38 +0100

    Delete DTOs directory

--- DTOs/TrickBidDTO.java
+++
@@ -1,6 +0,0 @@
-package JC.Training.src.WizardGame.DTOs;
-
-import JC.Training.src.WizardGame.enums.TrickBidExists;
-
-public record TrickBidDTO(TrickBidExists trickBidExists, Integer trickBid) {
-}
