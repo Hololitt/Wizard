ace64c316b3f1ec3aa6522ac71e00a6a8848249c
Nikon <109473540+Hololitt@users.noreply.github.com>
2026-02-02 20:44:29 +0100

    Delete models directory

--- models/GameResult.java
+++
@@ -1,6 +0,0 @@
-package JC.Training.src.WizardGame.models;
-
-import JC.Training.src.WizardGame.contexts.GameContext;
-
-public record GameResult(GameBot winnerBot, GameContext gameContext, int drawAmount) {
-}
