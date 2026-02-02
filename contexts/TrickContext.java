7b6c2266fc884d2d4cc171a8588dc0a2b0884904
Nikon <109473540+Hololitt@users.noreply.github.com>
2026-02-02 20:44:47 +0100

    Delete contexts directory

--- contexts/TrickContext.java
+++
@@ -1,9 +0,0 @@
-package JC.Training.src.WizardGame.contexts;
-
-import JC.Training.src.WizardGame.models.Card;
-import JC.Training.src.WizardGame.models.GameBot;
-
-import java.util.Map;
-
-public record TrickContext (Map<GameBot, Card> responses, Card firstDroppedCard, GameBot startingBot){
-}
